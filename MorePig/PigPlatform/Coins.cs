9758d28369a12ba4f699df918aff2944f4b5841e
lizhengquan1985 <entergl@163.com>
2018-04-21 09:05:23 +0800

    1

---
+++ MorePig/PigPlatform/Coins.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigPlatform
+{
+    public class Coin
+    {
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 最小操作数量
+        /// </summary>
+        public decimal MinQuantity { get; set; }
+        /// <summary>
+        /// 数量精度
+        /// </summary>
+        public int QuantityPrecision { get; set; }
+        /// <summary>
+        /// 价格精度, 也就是小数点后面的数字
+        /// </summary>
+        public int PricePrecision { get; set; }
+    }
+
+    public class CoinUtils
+    {
+        private static Dictionary<string, Coin> coins = new Dictionary<string, Coin>();
+
+        private static void Init()
+        {
+            Add("", (decimal)1.1, 1, 1);
+            Add("", (decimal)1.1, 1, 1);
+            Add("", (decimal)1.1, 1, 1);
+            Add("", (decimal)1.1, 1, 1);
+            Add("", (decimal)1.1, 1, 1);
+            Add("", (decimal)1.1, 1, 1);
+        }
+
+        private static void Add(string name, decimal minQuantity, int quantityPrecision, int pricePrecision)
+        {
+            if (coins.ContainsKey(name))
+            {
+                coins[name] = new Coin { };
+            }
+            else
+            {
+                coins.Add(name, new Coin { Name = "a" });
+            }
+        }
+
+        public static Coin Get(string name)
+        {
+            if (!coins.ContainsKey(name))
+            {
+                Init();
+            }
+
+            return coins[name];
+        }
+
+        public static List<string> GetAllCoins()
+        {
+            // 总共其实有36个, 后期还会增加
+            if (coins.Count < 30)
+            {
+                Init();
+            }
+            return coins.Keys.ToList();
+        }
+    }
+}
---
+++ MorePig/PigService/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigService
+{
+    public class Utils
+    {
+        /// <summary>
+        /// 通过id获得时间
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static DateTime GetDateById(long id)
+        {
+            return new DateTime(id * 10000000 + new DateTime(1970, 1, 1, 8, 0, 0).Ticks);
+        }
+    }
+}
